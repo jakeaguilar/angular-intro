d9b161ceee82c4f049025965abf1ceac900d0bd1
unknown <aguilarjacob23@gmail.com>
2019-05-23 18:04:00 -0700

    day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,47 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'angular-intro';
+  title: string = 'Hello World!';
+
+  age: number = 900000;
+  isShown: string = "I am being shown";
+  isNotShown: string = "I am not being shown";
+
+
+  person: object = {
+    name: "Jacob",
+    age: 25,
+    course: "Angular"
+  };
+
+  colors: string[] = [
+    'Black',
+    'Red',
+    'Purple'
+  ]
+
+  people: object[] = [
+    {
+      name: "Roy",
+      age: 25,
+      course: "Angular"
+    },
+    {
+      name: "Joe",
+      age: 26,
+      course: "Angular"
+    },
+    {
+      name: "Chris",
+      age: 45,
+      course: "Angular"
+    }
+  ]
+
+  myFunction(shouldShow: boolean): string {
+    return shouldShow ? this.isShown : this.isNotShown;
+  }
 }
